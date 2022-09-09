45f5420b8f6649fe7735ad053591a9322e013ad0
Monserrat <monserrat.jimenez2@gmail.com>
2022-09-09 14:32:30 -0500

    Upsert Page Handlers

---
+++ BookListRazor/BookListRazor/Pages/BookList/Upsert.cshtml.cs
@@ -0,0 +1,61 @@
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class UpsertModel : PageModel
+    {
+        private ApplicationDbContext _db;
+
+        public UpsertModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public async Task<IActionResult> OnGet(int? id)
+        {
+            Book = new Book();
+            if (id == null) //Id is null beacuse is a new Book
+            {
+                //Create
+                return Page(); // Return empty view for Create Book option
+            }
+            
+            //Update
+            Book = await _db.Book.FirstOrDefaultAsync(u => u.Id == id); //Same functionality as FindAsync()
+            if (Book == null)//id has value
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                if(Book.Id == 0)
+                {
+                    _db.Book.Add(Book);
+                }
+                else
+                {
+                    //Use Update() when you want to update every property of the Book
+                    //Otherwise, find the Book in DB and change the property you need manually
+                    _db.Book.Update(Book);
+                }
+
+                await _db.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+            return RedirectToPage();
+        }
+    }
+}
